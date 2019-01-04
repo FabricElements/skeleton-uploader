936a85142a1e38f966daf500562320d0642f9c0b
Erny Sans <ernysans@icloud.com>
2019-01-04 16:35:31 -0500

    Format and include file name

--- skeleton-uploader.js
+++ skeleton-uploader.js
@@ -4,6 +4,7 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-icon-button/paper-icon-button.js';
 import '@polymer/iron-icons/iron-icons.js';
+
 /**
  * `skeleton-uploader`
  *
@@ -18,98 +19,108 @@ class SkeletonUploader extends PolymerElement {
    */
   static get template() {
     return html`
-    <style>
-      :host {
-        display: block;
-        width: auto;
-        position: relative;
-      }
+      <style>
+        :host {
+          display: block;
+          width: auto;
+          position: relative;
+        }
 
-      #drop-area {
-        border-radius: 5px;
-        text-align: center;
-        cursor: pointer;
-      }
+        #drop-area {
+          border-radius: 5px;
+          text-align: center;
+          cursor: pointer;
+        }
 
-      #progress-bar {
-        background-color: var(--paper-green-500);
-        bottom: 0;
-        left: 0;
-        opacity: 1;
-        pointer-events: none;
-        position: absolute;
-        top: 0;
-        transition: all 500ms linear;
-        z-index: 10;
-      }
+        #progress-bar {
+          background-color: var(--paper-green-500);
+          bottom: 0;
+          left: 0;
+          opacity: 1;
+          pointer-events: none;
+          position: absolute;
+          top: 0;
+          transition: all 500ms linear;
+          z-index: 10;
+        }
 
-      #progress-bar.uploaded-true {
-        opacity: 0;
-        transition-duration: 500ms;
-      }
+        #progress-bar.uploaded-true {
+          opacity: 0;
+          transition-duration: 500ms;
+        }
 
-      [hidden] {
-        display: none;
-      }
+        [hidden] {
+          display: none;
+        }
 
-      paper-button {
-        color: white;
-        transition: transform 500ms linear;
-      }
+        paper-button {
+          color: white;
+          transition: transform 500ms linear;
+        }
 
-      .uploaderButton:active {
-        transform: scale(0.9);
-      }
+        .uploaderButton:active {
+          transform: scale(0.9);
+        }
 
-      paper-button iron-icon {
-        margin-right: 10px;
-      }
+        paper-button iron-icon {
+          margin-right: 10px;
+        }
 
-      paper-button.default {
-        background-color: var(--paper-blue-500);
-      }
+        paper-button.default {
+          background-color: var(--paper-blue-500);
+        }
 
-      paper-button.uploading {
-        background-color: var(--paper-grey-500);
-      }
+        paper-button.uploading {
+          background-color: var(--paper-grey-500);
+        }
 
-      paper-button.done {
-        background-color: var(--paper-green-500);
-      }
+        paper-button.done {
+          background-color: var(--paper-green-500);
+        }
 
-      paper-button.failed {
-        background-color: var(--paper-red-500);
-      }
+        paper-button.failed {
+          background-color: var(--paper-red-500);
+        }
 
-      paper-icon-button {
-        color: var(--paper-red-500);
-      }
+        paper-icon-button {
+          color: var(--paper-red-500);
+        }
 
-      paper-button[disabled] {
-        background-color: var(--paper-grey-400);
-      }
-    </style>
-    <input id="media-capture"
-           type="file"
-           accept="[[accept]]"
-           on-change="_upload"
-           hidden
-           size="5242880"
-           disabled$="[[isUploadDisabled]]">
-      <paper-button class$="[[buttonState]]
+        paper-button[disabled] {
+          background-color: var(--paper-grey-400);
+        }
+      </style>
+      <input
+        id="media-capture"
+        type="file"
+        accept="[[accept]]"
+        on-change="_upload"
+        hidden
+        size="5242880"
+        disabled$="[[isUploadDisabled]]"
+      />
+      <paper-button
+        class$="[[buttonState]]
                     uploaderButton"
-                    disabled$="[[disabled]]"
-                    on-tap="_tapButton">
+        disabled$="[[disabled]]"
+        on-tap="_tapButton"
+      >
         <div id="drop-area">
-            <div id="progress-bar"
-                 class$="uploaded-[[uploaded]]"
-                 style$="width:[[uploadProgress]]%;"></div>
-            <iron-icon icon="[[buttonIcon]]"></iron-icon>
-            [[buttonText]]
+          <div
+            id="progress-bar"
+            class$="uploaded-[[uploaded]]"
+            style$="width:[[uploadProgress]]%;"
+          ></div>
+          <iron-icon icon="[[buttonIcon]]"></iron-icon>
+          [[buttonText]]
         </div>
       </paper-button>
-      <paper-icon-button icon="cancel" hidden$="[[!showCancel]]" on-tap="_resetButton"></paper-icon-button>
-`;
+      <paper-icon-button
+        icon="cancel"
+        hidden$="[[!showCancel]]"
+        on-tap="_resetButton"
+      ></paper-icon-button>
+    `;
   }
 
   /**
@@ -251,11 +262,12 @@ class SkeletonUploader extends PolymerElement {
    * @private
    */
   _upload(event, fileObject) {
-    const file = fileObject ?
-      fileObject :
-      this.shadowRoot.querySelector('#media-capture').files[0];
-    const fileSize = this.shadowRoot.querySelector('#media-capture').files[0].size;
-    if (this.maxSize != 0 && fileSize > this.maxSize) {
+    const file = fileObject
+      ? fileObject
+      : this.shadowRoot.querySelector('#media-capture').files[0];
+    const fileSize = this.shadowRoot.querySelector('#media-capture').files[0]
+      .size;
+    if (this.maxSize !== 0 && fileSize > this.maxSize) {
       this.buttonState = 'failed';
       this._dispatchEvent('error', 'File size is bigger than specified');
       return;
@@ -266,7 +278,8 @@ class SkeletonUploader extends PolymerElement {
       return;
     }
     this.downloadURL = null;
-    this.titlePreview = file.name;
+    const fileName = file.name;
+    this.titlePreview = fileName;
     let fileExt = /\.[\w]+/.exec(file.name);
     const storageRef = firebase.storage().ref(this.path + fileExt);
     let metadataObject = null;
@@ -280,37 +293,43 @@ class SkeletonUploader extends PolymerElement {
       return;
     }
     this.task = storageRef.put(file, metadataObject);
-    this.task.on('state_changed', (snapshot) => {
-      // Observe state change events such as progress, pause, and resume
-      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      this.uploadProgress = progress;
-      this.buttonState = 'uploading';
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          this._dispatchEvent('paused', 'Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          this._dispatchEvent('running', 'Upload is running');
-          break;
-      }
-    }, (error) => {
-      error.code === 'storage/canceled' ?
-        this.buttonState = 'default' :
-        this.buttonState = 'failed';
-      // Handle unsuccessful uploads
-      this._dispatchEvent('error', error);
-    }, () => {
-      this.task.snapshot.ref.getDownloadURL().then((downloadURL) => {
-        // Handle successful uploads on complete
-        this.downloadURL = downloadURL;
-        this.extension = fileExt[0];
-        this._dispatchEvent('completed', {
-          type: file.type,
-          url: this.downloadURL,
+    this.task.on(
+      'state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        this.uploadProgress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.buttonState = 'uploading';
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            this._dispatchEvent('paused', 'Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            this._dispatchEvent('running', 'Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        error.code === 'storage/canceled'
+          ? (this.buttonState = 'default')
+          : (this.buttonState = 'failed');
+        // Handle unsuccessful uploads
+        this._dispatchEvent('error', error);
+      },
+      () => {
+        this.task.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          // Handle successful uploads on complete
+          this.downloadURL = downloadURL;
+          this.extension = fileExt[0];
+          this._dispatchEvent('completed', {
+            type: file.type,
+            url: this.downloadURL,
+            name: fileName,
+          });
+          this.buttonState = 'done';
         });
-        this.buttonState = 'done';
-      });
-    });
+      }
+    );
   }
 
   /**
@@ -319,11 +338,15 @@ class SkeletonUploader extends PolymerElement {
    * @return {*}
    */
   removeFile() {
-    const pathRef = firebase.storage().ref().child(this.path + this.extension);
-      // Get the download URL
-    return pathRef.delete()
-      .then(() => this.src = null)
-      .catch((error) => this.error = error);
+    const pathRef = firebase
+      .storage()
+      .ref()
+      .child(this.path + this.extension);
+    // Get the download URL
+    return pathRef
+      .delete()
+      .then(() => (this.src = null))
+      .catch((error) => (this.error = error));
   }
 
   /**
@@ -334,11 +357,13 @@ class SkeletonUploader extends PolymerElement {
    * @private
    */
   _dispatchEvent(event, detail) {
-    this.dispatchEvent(new CustomEvent(event, {
-      detail: detail,
-      bubbles: true,
-      composed: true,
-    }));
+    this.dispatchEvent(
+      new CustomEvent(event, {
+        detail: detail,
+        bubbles: true,
+        composed: true,
+      })
+    );
   }
 
   /**
